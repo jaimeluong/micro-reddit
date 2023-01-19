067a9d724a489da548eee2757561e82570909ebb
Jaime Luong <jaime.luong@gmail.com>
2023-01-19 00:48:28 -0600

    Create Post model with validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     validates :username, presence: true, length: { in: 4..16 }, uniqueness: true
     validates :password, presence: true, length: { in: 6..20 }
     validates :email, presence: true, length: { minimum: 5 }, uniqueness: true, format: { with: /\A\S+@.+\.\S+\z/, message: "Must be in email format" }
+
+    has_many :posts
 end
