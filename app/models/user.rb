aba4a7e96bcb1f247dd388cb6666d4ef0e9214ca
Jaime Luong <jaime.luong@gmail.com>
2023-01-19 01:00:21 -0600

    Create Comment model with validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
     validates :email, presence: true, length: { minimum: 5 }, uniqueness: true, format: { with: /\A\S+@.+\.\S+\z/, message: "Must be in email format" }
 
     has_many :posts
+    has_many :comments
 end
